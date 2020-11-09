dbf8de4ad4dca6d0b7f3f74e0033880512b6474b
cep104 <candicelizabethpeters@gmail.com>
2020-11-09 14:54:04 -0600

    commit tree 98379483af05b2e96844f93734d255172f564b57

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  index = 0 #index number
+  while index < array.length do  
+    #while inex number is less than the length of the array
+    yield(array[index])
+    #yield the arrays index number
+    index += 1
+    #add one to the index number
+  end
+  array #returns origial array
 end
